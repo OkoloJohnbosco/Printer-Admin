630ed7265554e1571b8390ede25d964638c97361
Johnbosco Okolo <john.bosco@mukuru.com>
2025-08-26 11:55:36 +0100

    ci: updated ci config

---
+++ commitlint.config.ts
@@ -0,0 +1,152 @@
+import type { UserConfig } from "@commitlint/types";
+
+const Configuration: UserConfig = {
+  extends: ["@commitlint/config-conventional"],
+  parserPreset: "conventional-changelog-atom",
+  formatter: "@commitlint/format",
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat", // New feature
+        "fix", // Bug fix
+        "docs", // Documentation changes
+        "style", // Changes that do not affect the meaning of the code (white-space, formatting, etc.)
+        "refactor", // Code changes that neither fix a bug nor add a feature
+        "perf", // Performance improvement
+        "test", // Adding missing tests or correcting existing tests
+        "build", // Changes that affect the build system or external dependencies (example scopes: npm)
+        "ci", // Changes to CI configuration files and scripts
+        "chore", // Other changes that don't modify src or test files
+        "revert", // Reverts a previous commit
+      ],
+    ],
+    "scope-enum": [
+      2,
+      "always",
+      [
+        "setup", // Project setup
+        "config", // Configuration files
+        "deps", // Dependency updates
+        "feature", // Feature-specific changes
+        "bug", // Bug fixes
+        "docs", // Documentation
+        "style", // Code style/formatting
+        "refactor", // Code refactoring
+        "test", // Tests
+        "build", // Build scripts or configuration
+        "ci", // Continuous integration
+        "release", // Release related changes
+        "other", // Other changes
+      ],
+    ],
+  },
+  helpUrl:
+    "https://github.com/conventional-changelog/commitlint/#what-is-commitlint",
+  /*
+   * Custom prompt configs
+   */
+  prompt: {
+    messages: {},
+    questions: {
+      type: {
+        description: "Select the type of change that you're committing",
+        enum: {
+          feat: {
+            description: "A new feature",
+            title: "Features",
+            emoji: "✨",
+          },
+          fix: {
+            description: "A bug fix",
+            title: "Bug Fixes",
+            emoji: "🐛",
+          },
+          docs: {
+            description: "Documentation only changes",
+            title: "Documentation",
+            emoji: "📚",
+          },
+          style: {
+            description:
+              "Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)",
+            title: "Styles",
+            emoji: "💎",
+          },
+          refactor: {
+            description:
+              "A code change that neither fixes a bug nor adds a feature",
+            title: "Code Refactoring",
+            emoji: "📦",
+          },
+          perf: {
+            description: "A code change that improves performance",
+            title: "Performance Improvements",
+            emoji: "🚀",
+          },
+          test: {
+            description: "Adding missing tests or correcting existing tests",
+            title: "Tests",
+            emoji: "🚨",
+          },
+          build: {
+            description:
+              "Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)",
+            title: "Builds",
+            emoji: "🛠",
+          },
+          ci: {
+            description:
+              "Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)",
+            title: "Continuous Integrations",
+            emoji: "⚙️",
+          },
+          chore: {
+            description: "Other changes that don't modify src or test files",
+            title: "Chores",
+            emoji: "♻️",
+          },
+          revert: {
+            description: "Reverts a previous commit",
+            title: "Reverts",
+            emoji: "🗑",
+          },
+        },
+      },
+      scope: {
+        description:
+          "What is the scope of this change (e.g. component or file name)",
+      },
+      subject: {
+        description:
+          "Write a short, imperative tense description of the change",
+      },
+      body: {
+        description: "Provide a longer description of the change",
+      },
+      isBreaking: {
+        description: "Are there any breaking changes?",
+      },
+      breakingBody: {
+        description:
+          "A BREAKING CHANGE commit requires a body. Please enter a longer description of the commit itself",
+      },
+      breaking: {
+        description: "Describe the breaking changes",
+      },
+      isIssueAffected: {
+        description: "Does this change affect any open issues?",
+      },
+      issuesBody: {
+        description:
+          "If issues are closed, the commit requires a body. Please enter a longer description of the commit itself",
+      },
+      issues: {
+        description: 'Add issue references (e.g. "fix #123", "re #123".)',
+      },
+    },
+  },
+};
+
+export default Configuration;
