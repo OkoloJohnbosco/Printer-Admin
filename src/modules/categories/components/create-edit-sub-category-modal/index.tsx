d9ac9d4165aae1d800287987f45ee62226e3be9d
Johnbosco Okolo <john.bosco@mukuru.com>
2025-10-29 11:41:11 +0100

    fix: update

--- src/modules/categories/components/create-edit-sub-category-modal/index.tsx
+++ src/modules/categories/components/create-edit-sub-category-modal/index.tsx
@@ -61,6 +61,7 @@ export default function CreateEditSubCategoryModal({
     toggleSubCategory
       .mutateAsync({
         name: data.name,
+        description: data.description,
         categoryId,
       })
       .then(() => {
