6f517122f630608f5b33a0bae764f012ffc4670a
Johnbosco Okolo <chukwuemekaokolo233@gmail.com>
2026-04-09 12:11:27 +0100

    fix: removed revenue stats cards

--- src/modules/revenue-and-payout/index.tsx
+++ src/modules/revenue-and-payout/index.tsx
@@ -2,13 +2,12 @@
 
 import RevenueHeader from "./components/revenue-header";
 import RecentTransactions from "./templates/recent-transaction";
-import RevenueStatsCardRow from "./templates/revenue-stats-card-row";
 
 export default function RevenueAndPayoutsPageTemplate() {
   return (
     <div className="page-fade-in space-y-5">
       <RevenueHeader />
-      <RevenueStatsCardRow />
+      {/* <RevenueStatsCardRow /> */}
       <RecentTransactions />
     </div>
   );
