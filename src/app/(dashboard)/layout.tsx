5985cb30103499159921b98b79d726638bbe8ad3
Johnbosco Okolo <chukwuemekaokolo233@gmail.com>
2026-05-21 08:24:00 +0100

    fix: fixed bugs

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,9 +1,15 @@
+import Loader from "@/components/ui/loader";
 import DashboardLayoutWrapper from "@/layout/dashboard-layout";
+import { Suspense } from "react";
 
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <DashboardLayoutWrapper>{children}</DashboardLayoutWrapper>;
+  return (
+    <DashboardLayoutWrapper>
+      <Suspense fallback={<Loader />}>{children}</Suspense>
+    </DashboardLayoutWrapper>
+  );
 }
