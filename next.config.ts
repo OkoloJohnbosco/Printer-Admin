bbe26f2958ca6f06d6b1f8ffeb4eed6204adb643
Johnbosco Okolo <chukwuemekaokolo233@gmail.com>
2026-07-30 14:39:03 +0100

    chore: updated next config

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: NextConfig = {
       new URL("https://fly.storage.tigris.dev/**"),
       new URL("http://159.89.91.30:9000/**"),
       new URL("https://s3.printa.africa/**"),
+      new URL("https://s3.stagprinta.africa/**"),
     ],
   },
 };
